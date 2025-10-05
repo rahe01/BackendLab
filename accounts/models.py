4a9f4b94c919b246a1cd43a53bd6f8aee76b7452
rahe01 <rahatrahe01@gmail.com>
2025-10-05 23:35:18 +0600

    custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractBaseUser , BaseUserManager, PermissionsMixin
+from django.utils import timezone
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
+    def create_superuser(self, email, password=None, **extra_fields):
+
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Super user must have staff = True')
+        
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Super user must have superuser = True')
+        user = self.create_user(email, password, **extra_fields)
+
+        user.is_staff = True
+        user.is_superuser = True
+        user.is_active = True
+        user.save(using=self._db)
+        return user
